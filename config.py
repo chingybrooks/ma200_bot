0fd9dba1127ca9ea45f1be5096ea82589011ff41
Shyngys <73993784+chingybrooks@users.noreply.github.com>
2024-11-19 10:51:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Загружаем переменные окружения из .env файла
+# Загрузка переменных из .env файла
+load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
-BINANCE_API_KEY = os.getenv('BINANCE_API_KEY')
-BINANCE_API_SECRET = os.getenv('BINANCE_API_SECRET')
+class Config:
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    CHAT_ID = os.getenv('CHAT_ID')
