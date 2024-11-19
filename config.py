e5207cc7a8c94268aaef2264c59a7b51e4916203
Shyngys <73993784+chingybrooks@users.noreply.github.com>
2024-11-19 09:55:36 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+TELEGRAM_TOKEN = 'ваш_токен_бота'
+CHAT_ID = 'ID_чата_или_канала'  # Замените на ваш ID чата или канала
+BINANCE_API_KEY = 'ваш_бинанс_API_ключ'
+BINANCE_API_SECRET = 'ваш_бинанс_секретный_ключ'
