b9f97bdbe2cf084a4ce089bc9699b8ef383af808
Shyngys <73993784+chingybrooks@users.noreply.github.com>
2024-11-19 10:57:10 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Загрузка переменных из .env файла
-load_dotenv()
-
-class Config:
-    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-    CHAT_ID = os.getenv('CHAT_ID')
